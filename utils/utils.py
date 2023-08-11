2baeba2d247f2e1b40e41ed0e9e8971c8312368f
Asif Hanif <50404242+asif-hanif@users.noreply.github.com>
2023-08-11 12:49:32 +0400

    Update utils.py

--- utils/utils.py
+++ utils/utils.py
@@ -95,14 +95,8 @@ def print_attack_info(args):
     if args.attack_name=="fgsm":          print(f"\n FGSM: eps={args.eps} , targeted={args.targeted}\n")
     if args.attack_name=="bim":           print(f"\n BIM: alpha={args.alpha} , eps={args.eps} , steps={args.steps} , targeted={args.targeted}\n")
     if args.attack_name=="gn":            print(f"\n GN: std={args.std}\n")
-    if args.attack_name=="dfb":           print(f"\n DFB:  PatchSize={tuple(args.block_size)}, Freq_Band={args.freq_band}, Freq_Reg={args.freq_reg_mode}\n")
-    if args.attack_name=="dfb-2d":        print(f"\n DFB_2D:  PatchSize={tuple(args.block_size)}, Freq_Band={args.freq_band}, Freq_Reg={args.freq_reg_mode}\n")
-    if args.attack_name=="ssa-2d":        print(f"\n SSA_2D: rho={args.rho} , eps={args.eps} , num_spectrum_augs={args.num_spectrum_augs} , steps={args.steps}, patch_size={tuple(args.block_size)}\n")
-    if args.attack_name=="ssa-3d":        print(f"\n SSA_3D: rho={args.rho} , eps={args.eps} , num_spectrum_augs={args.num_spectrum_augs} , steps={args.steps}, patch_size={tuple(args.block_size)}\n")
     if args.attack_name=="vafa-2d":       print(f"\n VAFA_2D:  q_max={args.q_max} , steps={args.steps} , patch_size={tuple(args.block_size)}\n")
     if args.attack_name=="vafa-3d":       print(f"\n VAFA_3D:  q_max={args.q_max} , steps={args.steps} , patch_size={tuple(args.block_size)} , use_ssim_loss={args.use_ssim_loss}\n")
-    if args.attack_name=="spa":           print(f"\n SPA:  Rho={args.rho}, Steps={args.steps}, PatchSize={tuple(args.block_size)}, Freq_Band={args.freq_band}, LambdaDICE={args.lambda_dice}, UseSSIM={args.use_ssim_loss},  {'Lambda_SSIM='+str(args.lambda_ssim) if args.use_ssim_loss else ''},  Freq_Reg={args.freq_reg_mode}\n")
-    if args.attack_name=="spa-2d":        print(f"\n SPA_2D:  Rho={args.rho}, Steps={args.steps}, PatchSize={tuple(args.block_size)}, Freq_Band={args.freq_band}, LambdaDICE={args.lambda_dice}, UseSSIM={args.use_ssim_loss},  {'Lambda_SSIM='+str(args.lambda_ssim) if args.use_ssim_loss else ''},  Freq_Reg={args.freq_reg_mode}\n")
 
 
 def get_folder_name(args):
@@ -110,13 +104,7 @@ def get_folder_name(args):
     elif args.attack_name == "fgsm"   : folder_name = f"fgsm_eps_{args.eps}"
     elif args.attack_name == "bim"    : folder_name = f"bim_alpha_{args.alpha}_eps_{args.eps}_i_{args.steps}"
     elif args.attack_name == "gn"     : folder_name = f"gn_std_{args.std}"
-    elif args.attack_name == "dfb"    : folder_name = f"dfb_3d_dct_{args.block_size[0]}x{args.block_size[1]}x{args.block_size[2]}_drop_band_{args.freq_band}"
-    elif args.attack_name == "dfb-2d" : folder_name = f"dfb_2d_dct_{args.block_size[0]}x{args.block_size[1]}_drop_band_{args.freq_band}"  
-    elif args.attack_name == "ssa-2d":  folder_name = f"ssa_rho_{args.rho}_eps_{args.eps}_num_spectrum_augs_{args.num_spectrum_augs}_i_{args.steps}_2d_dct_{args.block_size[0]}x{args.block_size[1]}"
-    elif args.attack_name == "ssa-3d":  folder_name = f"ssa_rho_{args.rho}_eps_{args.eps}_num_spectrum_augs_{args.num_spectrum_augs}_i_{args.steps}_3d_dct_{args.block_size[0]}x{args.block_size[1]}x{args.block_size[2]}"
     elif args.attack_name == "vafa-2d": folder_name = f"vafa_q_max_{args.q_max}_i_{args.steps}_2d_dct_{args.block_size[0]}x{args.block_size[1]}"
     elif args.attack_name == "vafa-3d": folder_name = f"vafa_q_max_{args.q_max}_i_{args.steps}_3d_dct_{args.block_size[0]}x{args.block_size[1]}x{args.block_size[2]}_use_ssim_loss_{args.use_ssim_loss}"
-    elif args.attack_name == "spa"    : folder_name = f"spa_rho_{args.rho}_i_{args.steps}_3d_dct_{args.block_size[0]}x{args.block_size[1]}x{args.block_size[2]}_lambda_dice_{args.lambda_dice}{'_with_ssim_lambda_'+str(args.lambda_ssim) if args.use_ssim_loss else ''}{'_freq_reg' if args.freq_reg_mode else ''}"
-    elif args.attack_name == "spa-2d" : folder_name = f"spa_rho_{args.rho}_i_{args.steps}_2d_dct_{args.block_size[0]}x{args.block_size[1]}_lambda_dice_{args.lambda_dice}{'_with_ssim_lambda_'+str(args.lambda_ssim) if args.use_ssim_loss else ''}{'_freq_reg' if args.freq_reg_mode else ''}"
     else: raise ValueError(f"Attack '{args.attack_name}' is not implemented.")
     return folder_name
